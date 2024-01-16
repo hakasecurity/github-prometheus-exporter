caffb17d37988330a7a3d47aca5a1235e3f8e6d0
hxnir <nir@hakasecurity.com>
2024-01-16 16:23:19 +0200

    Add mypy and ruff

--- github_prometheus_exporter/github_prometheus_exporter/exporter.py
+++ github_prometheus_exporter/github_prometheus_exporter/exporter.py
@@ -1,17 +1,17 @@
+from pathlib import Path
+
 import github
 from github import Github
 from github.Repository import Repository
 
-PRIVATE_KEY_PATH = './aim-promhippie-exporter.2024-01-16.private-key.pem'
+PRIVATE_KEY_PATH = "./aim-promhippie-exporter.2024-01-16.private-key.pem"
 APPLICATION_ID = 798085
 INSTALLATION_ID = 46187215
 ORGANIZATION_ID = "hakasecurity"
 
 
 def get_authenticated_api() -> Github:
-    with open(PRIVATE_KEY_PATH) as private_key_file:
-        private_key = private_key_file.read()
-
+    private_key = Path(PRIVATE_KEY_PATH).read_text()
     integration = github.GithubIntegration(APPLICATION_ID, private_key)
     access_token = integration.get_access_token(INSTALLATION_ID).token
     return github.Github(access_token)
@@ -21,12 +21,11 @@ def get_all_repositories(g: Github) -> list[Repository]:
     return list(g.get_organization(ORGANIZATION_ID).get_repos())
 
 
-def main():
+def main() -> None:
     g = get_authenticated_api()
 
-    haka = g.get_repo('hakasecurity/haka')
-    print(get_all_repositories(g))
+    g.get_repo("hakasecurity/haka")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
